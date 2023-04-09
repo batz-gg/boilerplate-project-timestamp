2bc8cd1013e8cfab6ac11cb5c459767703ac05c9
batz-gg <batz.gg@gmail.com>
2023-04-09 19:56:17 +0800

    modified:   .gitignore 	modified:   index.js 	modified:   views/index.html

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // index.js
 // where your node app starts
-
+// require environment variables
 // init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,15 +18,31 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+  let date;
+  if (req.params.date === undefined) {
+    date = new Date();
+  } else if (!isNaN(req.params.date)) {
+    date = new Date(parseInt(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
 
+  if (isNaN(date.getTime())) {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(PORT, () => {
+  console.log(`Timestamp microservice is listening on port http://localhost:${listener.address().port}/`);
 });
